f77fb3f9787d870fe27286353f483a28eb4f7e79
JakeStew81 <jakestewart711@icloud.com>
2024-10-29 16:12:02 -0400

    Made Command Based template

---
+++ CommandBasedTemplate/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class Constants {
+
+  public class TemplateConstants {}
+
+  public class RobotConstants {
+    public static final int CONTROLLER_PORT = 1;
+    public static final double TRIGGER_DEADZONE = 0.15;
+
+    public static final String LOGGING_ROOT_PATH = "Robot";
+    public static final boolean LOGGING_FILE_ONLY = false;
+    public static final boolean LAZY_LOGGING = false;
+  }
+}
